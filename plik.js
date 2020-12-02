08982970410749090fa938624e24b9da036d1d4d
Seba Kiero <bondziorno4@wp.pl>
2020-12-02 12:11:17 +0100

    pierwsze

---
+++ plik.js
@@ -0,0 +1,54 @@
+
+// komentarz jednowierszowy
+
+/*
+komentarz
+wielowierszowy
+*/
+
+/*
+typy danych w JS
+JS jest językiem słabotypowanym
+number - typ numeryczny
+string - typ tekstowy
+boolean - typ logiczny
+obiekt - typ zlożony
+*/
+
+/*
+typ number:
+	- typ całkowity (integer, int)
+	- typ zmiennoprecinkowy(float)
+*/
+
+/*
+zmienne:
+
+tworzenie zmiennych:
+	let - tworzy zmienne lokalne ( dziające od { do })
+	var - tworzy zmienne globalne
+	
+	nazwa zmiennych -> zmienne mogą zaczynać się od litery lub _
+	wielkość liter ma znaczenie przy nazwach zmiennych i stłaych
+*/
+
+let zmienna = 2;
+let zmienna2 = "slowo";
+let zmienna3 = true;
+let zmienna4 = 3.6;
+
+zmienna = 50;
+
+/*
+stałe
+*/
+
+const STALA = 5;
+
+//STALA = 10; to jest błąd
+
+
+
+
+
+
